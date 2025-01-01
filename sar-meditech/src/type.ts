ebd93c5242ba3ea6eb88e30389382cd50aa3bc47
mantavya0807 <mantavyamahajan@gmail.com>
2025-01-01 18:43:12 -0500

    Fix TypeScript and Netlify build errors

---
+++ sar-meditech/src/type.ts
@@ -0,0 +1,17 @@
+// src/types.ts
+
+export interface Service {
+    title: string;
+    description: string;
+    image: string;
+    details: string;
+  }
+  
+  export interface Testimonial {
+    id: number;
+    name: string;
+    position: string;
+    message: string;
+    photo: string;
+  }
+  
